86bde05aa2f6ce1ea683ad32899fda5e58480128
Alberto Escobar <128758041+FernandoHernandezCruz@users.noreply.github.com>
2023-12-22 11:03:08 -0600

    Update help.component.ts

--- src/app/folder/help/help.component.ts
+++ src/app/folder/help/help.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslationService } from 'src/app/services/translation.service';
 
+interface AlertInput {
+  label: string;
+  type: string;
+  value: string;
+  checked?: boolean; 
+}
+
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
@@ -9,7 +16,7 @@ import { TranslationService } from 'src/app/services/translation.service';
 export class HelpComponent implements OnInit {
   currentLanguage = 'es';
   public alertButtons = ['OK'];
-  public alertInputs = [
+  public alertInputs: AlertInput[] = [
     {
       label: 'Español',
       type: 'radio',
@@ -21,7 +28,10 @@ export class HelpComponent implements OnInit {
       value: 'en',
     },
   ];
-  constructor(public translationService: TranslationService) { }
+  constructor(public translationService: TranslationService) { 
+    this.currentLanguage = translationService.currentLanguage;
+    this.setDefaultLanguage();
+  }
 
   ngOnInit() {
   }
@@ -29,5 +39,15 @@ export class HelpComponent implements OnInit {
   changeLanguage = (data: any) => {
     this.currentLanguage = data;
     this.translationService.setLanguage(this.currentLanguage);
+  }
+  
+  private setDefaultLanguage() {
+    const storedLanguage = localStorage.getItem('selectedLanguage');
+    if (storedLanguage && this.alertInputs.find(input => input.value === storedLanguage)) {
+      this.currentLanguage = storedLanguage;
+      this.alertInputs.forEach(input => {
+        input.checked = input.value === this.currentLanguage;
+      });
+    }
   }
 }
